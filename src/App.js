c666eb44f445ad2fbd935375e99bd97999de5dc6
semi-infiknight <mathurshubhang2002@gmail.com>
2024-03-11 11:49:40 +0530

    pwa-app-config

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
-import "./App.css";
-import Main from "./pages/Main/Main";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AllChallenges from "./pages/AllChallenges/AllChallenges";
-import store from "./redux/store/store";
-import { Provider } from "react-redux";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { LedgerWalletAdapter } from "@solana/wallet-adapter-ledger";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
-import { TorusWalletAdapter } from "@solana/wallet-adapter-torus";
-import { clusterApiUrl } from "@solana/web3.js";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { useMemo } from "react";
-
 function App() {
-  const solNetwork = WalletAdapterNetwork.Mainnet;
-  const endpoint = useMemo(() => clusterApiUrl(solNetwork), [solNetwork]);
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ solNetwork }),
-      new TorusWalletAdapter(),
-      new LedgerWalletAdapter(),
-    ],
-    [solNetwork]
-  );
-
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
-        <WalletModalProvider>
-          <Provider store={store}>
-            <Router>
-              <Main>
-                <Routes>
-                  <Route path="/" element={<AllChallenges />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/explore" element={<AllChallenges/>} />
-                </Routes>
-              </Main>
-            </Router>
-          </Provider>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div className="text-center">
+      <header className="bg-blue-500  flex flex-col justify-center text-white">
+        <p className="text-3xl">
+          Hello
+        </p>
+        <a
+          className="text-blue-200 hover:text-red-500"
+          href="https://tailwindcss.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+       Catoff here
+        </a>
+      </header>
+    </div>
   );
 }
 
