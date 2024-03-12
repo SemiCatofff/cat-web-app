c982b92a7b60c443ad00d3a08b5ff583f52b7783
IshitaSrivast <20cs3034@rgipt.ac.in>
2024-03-12 23:49:56 +0530

    apiCalls-dashboard

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,52 @@
-function Login() {
-  return (
+import { setLoginState } from "../../redux/actions/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { loginAPI } from "../../utils/OktoApiCalls";
+import axios from "axios";
 
-    <div className="w-screen h-screen flex justify-center">
-    <div className="w-32 h-10 bg-blue-500 flex justify-center cursor-pointer">
-      Login
+import { useGoogleLogin } from "@react-oauth/google";
+
+function Login() {
+  const dispatch = useDispatch();
+  const handleLogin = async () => {
+    const output = await loginAPI();
+  };
+
+  
+
+ 
+  
+
+  const login = useGoogleLogin({
+    onSuccess: async tokenResponse => {
+      console.log(tokenResponse);
+      // fetching userinfo can be done on the client or the server
+      // const GoogleAuth = window.gapi.auth2.getAuthInstance();
+      // console.log("=================================================");
+      // const googleUser = await GoogleAuth.signIn();
+      // console.log('Token || ' + googleUser.getAuthResponse().id_token);
+      const userInfo = await axios
+        .get('https://www.googleapis.com/oauth2/v3/userinfo', {
+          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+        })
+        .then(res => res.data);
+
+      console.log(userInfo);
+    },
+    // flow: 'implicit', // implicit is the default
+  });
+
+
+
+
+  return (
+    <div className="w-1/2 h-1/2 flex justify-center items-center">
+      <div
+        className="w-32 h-10 bg-blue-500 flex justify-center items-center cursor-pointer rounded-lg"
+        onClick={()=>{login(); handleLogin();}}
+      >
+        Login
+      </div>
     </div>
-  </div>
   );
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,24 +1,22 @@
 import { useSelector, useDispatch } from "react-redux";
 import Login from "../Login/Login";
+import Navbar from "../../components/Navbar/Navbar";
 
 function Main(props) {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
   return (
-    <div className="main-page">
-      {/* {isLoggedIn ? (
-        <> */}
-        
-          <div className="w-screen h-screen flex align-center justify-center">
-            <div className="w-screen h-screen flex align-center justify-center"></div>
-            {props.children}
-          </div>
-        {/* </>
-      ) : (
+    <div className="w-screen h-screen flex align-center justify-center items-center text-white">
+      {isLoggedIn ? (
         <>
-          <Login />
-        </> */}
-      {/* )} */}
+          <div className="w-screen h-screen flex flex-col">
+            <div className="w-full h-auto">{props.children}</div>{" "}
+          </div>
+          <Navbar />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
