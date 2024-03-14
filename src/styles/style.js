ac99fb0a5dfe9e19620b2ea3212b8abc3c609ef0
semi-infiknight <mathurshubhang2002@gmail.com>
2024-03-14 20:35:53 +0530

    add: home page components + stylings + Challenge card component slider.

--- src/styles/style.js
+++ src/styles/style.js
@@ -1,18 +1,19 @@
 const styles = {
-  heading1: "text-[24px] font-ttruns font-bold",
-  heading2: "text-[16px] font-ttruns font-bold",
-  subheading: "text-[14px] font-inter font-semibold",
-  subtext: "text-[14px] font-inter font-regular",
-  paragraph: "text-[12px] font-inter font-regular",
+  heading1: "text-[22px] font-ttruns font-bold text-white",
+  heading2: "text-[16px] font-ttruns font-bold text-white",
+  subheading: "text-[14px] font-inter font-semibold text-white",
+  subtext: "text-[14px] font-inter font-regular text-white",
+  paragraph: "text-[12px] font-inter font-regular text-white",
 
-  caption1: "text-[12px] font-inter font-semibold",
-  caption2: "text-[10px] font-inter font-semibold",
-  buttoncta1: "text-[16px] font-inter font-semibold",
-  buttoncta2: "text-[16px] font-ttruns font-semibold",
+  caption1: "text-[12px] font-inter font-semibold text-white",
+  caption2: "text-[10px] font-inter font-semibold text-white",
+  buttoncta1: "text-[16px] font-inter font-semibold text-white",
+  buttoncta2: "text-[16px] font-ttruns font-semibold text-white",
 
   flexCenter: "flex justify-center items-center",
   flexStart: "flex justify-center items-start",
   flexEnd: "flex  items-end",
+  flexBetween: "flex justify-between",
 
   paddingX: "px-4",
   paddingY: "py-4",
@@ -21,7 +22,7 @@ const styles = {
 
   marginX: "mx-4",
   marginL: "ml-4",
-  marginY: "my-4",
+  marginY: "my-2",
 };
 
 export default styles;
