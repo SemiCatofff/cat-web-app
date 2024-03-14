2fbb8a6ab8cb5ecec65dab8d87a9e8ea99348b8c
swetakarar <kararsweta@gmail.com>
2024-03-14 10:16:25 +0530

    add: fonts config

---
+++ src/styles/style.js
@@ -0,0 +1,27 @@
+const styles = {
+  // font-ttruns font-bold
+  heading1: "",
+  heading2: "",
+  subheading: "",
+  subtext: "",
+  paragraph: "",
+
+  caption1: "",
+  caption2: "",
+  buttoncta: "",
+
+  flexCenter: "flex justify-center items-center",
+  flexStart: "flex justify-center items-start",
+  flexEnd: "flex  items-end",
+
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  paddingL: "sm:pl-16 pl-6",
+  padding: "sm:px-16 px-6 sm:py-12 py-4",
+
+  marginX: "sm:mx-16 mx-6",
+  marginL: "sm:ml-16 ml-6",
+  marginY: "sm:my-16 my-6",
+};
+
+export default styles;
