758dbf3a579ee0f175b1702ae66bba05c7b9f612
swetakarar <kararsweta@gmail.com>
2024-03-14 21:00:25 +0530

    fix: appbar routing + linked challenge components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,50 +10,77 @@ function Navbar() {
   const navigate = useNavigate();
   const [tab, setTab] = useState(0);
 
-  
+  const routes = ["/", "/create", "/dashboard", "/settings"];
+
+  const handleTabClick = (index) => {
+    setTab(index);
+    navigate(routes[index]);
+  };
 
   return (
     <footer className="mb-4 mx-4 bottom-0 h-custom bg-custom rounded-box absolute left-0 right-0">
       <div className="flex h-full justify-center items-center gap-[12%]">
-        <button className="flex flex-col items-center" onClick={()=>{setTab(0)}}>
+        <button
+          className="flex flex-col items-center"
+          onClick={() => handleTabClick(0)}
+        >
           <span>
             {" "}
             <img src={category}></img>
           </span>
           <span className="text-xs text-yellow">Explore</span>
-          {tab === 0 &&<span className="absolute bottom-[-1px]">
-            <img src={sel}></img>
-          </span>}
+          {tab === 0 && (
+            <span className="absolute bottom-[-1px]">
+              <img src={sel}></img>
+            </span>
+          )}
         </button>
-        <button className="flex flex-col items-center gap-1" onClick={()=>{setTab(1)}}>
+        <button
+          className="flex flex-col items-center gap-1"
+          onClick={() => handleTabClick(1)}
+        >
           <span>
             {" "}
             <img src={add}></img>
           </span>
           <span className="text-xs text-yellow">Create</span>
-          {tab === 1 && <span className="absolute bottom-[-1px]">
-            <img src={sel}></img>
-          </span>}
+          {tab === 1 && (
+            <span className="absolute bottom-[-1px]">
+              <img src={sel}></img>
+            </span>
+          )}
         </button>
-        <button className="flex flex-col items-center gap-1" onClick={()=>{setTab(2)}}>
+        <button
+          className="flex flex-col items-center gap-1"
+          onClick={() => handleTabClick(2)}
+        >
           <span>
             {" "}
             <img src={profile}></img>
           </span>
           <span className="text-xs text-yellow">Profile</span>
-          {tab === 2 && <span className="absolute bottom-[-1px]">
-            <img src={sel}></img>
-          </span>}
+          {tab === 2 && (
+            <span className="absolute bottom-[-1px]">
+              <img src={sel}></img>
+            </span>
+          )}
         </button>
-        <button className="flex flex-col items-center gap-1" onClick={()=>{setTab(3)}}>
+        <button
+          className="flex flex-col items-center gap-1"
+          onClick={() => {
+            handleTabClick(4);
+          }}
+        >
           <span>
             {" "}
             <img src={setting}></img>
           </span>
           <span className="text-xs text-yellow">Settings</span>
-          {tab === 3 && <span className="absolute bottom-[-1px]">
-            <img src={sel}></img>
-          </span>}
+          {tab === 3 && (
+            <span className="absolute bottom-[-1px]">
+              <img src={sel}></img>
+            </span>
+          )}
         </button>
       </div>
     </footer>
