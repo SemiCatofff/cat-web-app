b47f3a9b54903c7275959c36cb8bda29a3c22702
swetakarar <kararsweta@gmail.com>
2024-03-18 13:52:22 +0530

    add: profile section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,25 +5,6 @@ const Navbar = () => {
   const overlayRef = useRef(null);
   const bodyRef = useRef(null);
 
-  //   useEffect(() => {
-  //     const buttonContainer = buttonContainerRef.current;
-  //     const overlay = overlayRef.current;
-  //     const body = bodyRef.current;
-
-  //     const toggleClass = () => {
-  //       buttonContainer.classList.toggle("active");
-  //       overlay.classList.toggle("open");
-  //       body.classList.toggle("active");
-  //     };
-
-  //     buttonContainer.addEventListener("click", toggleClass);
-
-  //     // Clean up the event listener when the component unmounts
-  //     return () => {
-  //       buttonContainer.removeEventListener("click", toggleClass);
-  //     };
-  //   }, []);
-
   useEffect(() => {
     const buttonContainer = buttonContainerRef.current;
     const overlay = overlayRef.current;
@@ -49,45 +30,54 @@ const Navbar = () => {
           <a href="/" className=" z-40">
             <img src={""} alt="Logo" className="w-full" />
           </a>
-          <div id="menuToggle  z-10">
-            <div ref={buttonContainerRef} class="button_container1 z-30">
-              <span class="top1"></span>
-              <span class="middle"></span>
-              <span class="bottom1"></span>
+          <div className="flex justify-between ">
+            <div className=" mr-4 z-10">
+              <img
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="rounded-full bg-slate-500  w-10 h-10 object-cover"
+              />
             </div>
+            <div id="menuToggle  z-10">
+              <div ref={buttonContainerRef} class="button_container1 z-30">
+                <span class="top1"></span>
+                <span class="middle"></span>
+                <span class="bottom1"></span>
+              </div>
 
-            <div ref={overlayRef} class="overlay mt-0">
-              <div class="nav">
-                <div className="flex justify-end px-4 w-full text-white">
-                  <div className="col2 text-right">
-                    {" "}
-                    <ul>
-                      <div className="nav-para text-2xl mb-4 mt-4">
-                        <a className="lala  relative " href="/anarchy">
-                          Explore
-                        </a>
-                      </div>
-                      <div className="nav-para text-2xl mb-4 mt-4">
-                        <a className="lala  relative " href="/anarchy">
-                          Create Challenge
-                        </a>
-                      </div>
-                      <div className="nav-para text-2xl mb-4 mt-4">
-                        <a className="lala  relative " href="/anarchy">
-                          Join Challenge
-                        </a>
-                      </div>
-                      <div className="nav-para text-2xl mb-4 mt-4">
-                        <a className="lala  relative " href="/anarchy">
-                          My Profile
-                        </a>
-                      </div>
-                      <div className="nav-para text-2xl mb-4 mt-4">
-                        <a className="lala  relative " href="/anarchy">
-                          Invite Friend
-                        </a>
-                      </div>
-                    </ul>
+              <div ref={overlayRef} class="overlay mt-0">
+                <div class="nav">
+                  <div className="flex justify-end px-4 w-full text-white">
+                    <div className="col2 text-right">
+                      {" "}
+                      <ul>
+                        <div className="nav-para text-2xl mb-4 mt-4">
+                          <a className="lala  relative " href="/anarchy">
+                            Explore
+                          </a>
+                        </div>
+                        <div className="nav-para text-2xl mb-4 mt-4">
+                          <a className="lala  relative " href="/anarchy">
+                            Create Challenge
+                          </a>
+                        </div>
+                        <div className="nav-para text-2xl mb-4 mt-4">
+                          <a className="lala  relative " href="/anarchy">
+                            Join Challenge
+                          </a>
+                        </div>
+                        <div className="nav-para text-2xl mb-4 mt-4">
+                          <a className="lala  relative " href="/anarchy">
+                            My Profile
+                          </a>
+                        </div>
+                        <div className="nav-para text-2xl mb-4 mt-4">
+                          <a className="lala  relative " href="/anarchy">
+                            Invite Friend
+                          </a>
+                        </div>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
