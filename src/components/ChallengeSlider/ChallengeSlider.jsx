809e8cbd99feddcddd5586bf3e9af5c646d46bd8
semi-infiknight <mathurshubhang2002@gmail.com>
2024-04-06 12:54:14 +0530

    fix: single challenge alignment issues in the ongoin challenge slider

--- src/components/ChallengeSlider/ChallengeSlider.jsx
+++ src/components/ChallengeSlider/ChallengeSlider.jsx
@@ -1,57 +1,59 @@
 import Slider from 'react-slick'
 import challenge from '../../assets/images/challe.png'
-import { useState } from 'react'
 import styles from '../../styles/style'
 import moment from 'moment'
 
-const ChallengeSlider=({items}) =>{
-    var settings = {
-        infinite: false,
-        speed: 500,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        slidesToShow: 2.15,
-        slidesToScroll: 2,
-      }
+const ChallengeSlider = ({ items }) => {
+  var settings = {
+    infinite: false,
+    speed: 500,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    slidesToShow: 2.15,
+    slidesToScroll: 2,
+  }
 
+  // const sliderContainerClasses = `slider-container ${
+  //   items.length === 1 ? 'single-item' : ''
+  // }`
 
-    return( <div className={` slider-container overflow-hidden `}>
-    {items.length > 0 && <Slider {...settings}>
-      {items.map((item) => {
-        return (
-          <div className="relative w-full cursor-pointer">
-            <img
-              src={challenge}
-              alt=""
-              className="w-[200px]"
-            />
+  return (
+    <div className={` slider-container `}>
+      <Slider {...settings}>
+        {items.map((item) => {
+          return (
+            <div className="relative w-full cursor-pointer">
+              <img src={challenge} alt="" className="w-[200px]" />
 
-            <div
-              className={`absolute top-5 left-5 bg-white rounded-[80px] flex items-center justify-center `}
-            >
-              <div className={`${styles.flexBetween} !text-[11px] !text-[#6F6F6F] px-2 py-1`}>
-                {' '}
-                {moment
-                  .duration(
-                    moment(parseInt(item.EndDate)).diff(
-                      moment(parseInt(item.StartDate))
+              <div
+                className={`absolute top-5 left-5 bg-white rounded-[80px] flex items-center justify-center `}
+              >
+                <div
+                  className={`${styles.flexBetween} !text-[11px] !text-[#6F6F6F] px-2 py-1`}
+                >
+                  {' '}
+                  {moment
+                    .duration(
+                      moment(parseInt(item.EndDate)).diff(
+                        moment(parseInt(item.StartDate))
+                      )
                     )
-                  )
-                  .humanize()}
+                    .humanize()}
+                </div>
+              </div>
+              <div className={`absolute bottom-5 left-5 h-[22px]`}>
+                <div
+                  className={`${styles.flexBetween} px-2 !text-[10px] !text-[#ffffff]`}
+                >
+                  {item.ChallengeName}
+                </div>
               </div>
             </div>
-            <div className={`absolute bottom-5 left-5 h-[22px]`}>
-              <div className={`${styles.flexBetween} px-2 !text-[10px] !text-[#ffffff]`}>
-                {item.ChallengeName}
-                
-              </div>
-            </div>
-          </div>
-        )
-      })}
-    </Slider>}
-  </div>)
+          )
+        })}
+      </Slider>
+    </div>
+  )
 }
-
 
 export default ChallengeSlider
