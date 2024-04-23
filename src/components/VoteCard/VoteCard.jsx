6f6c99b36ae11ba93f69a67f08d103ac4695a2ab
IshitaSrivast <20cs3034@rgipt.ac.in>
2024-04-23 15:30:18 +0530

    ui/validator_ui

--- src/components/VoteCard/VoteCard.jsx
+++ src/components/VoteCard/VoteCard.jsx
@@ -1,43 +1,81 @@
 import card2 from '../../assets/images/img.png'
 import styles from '../../styles/style'
+import { useState } from 'react'
 
-const VoteCard = ({name,img,time, isChecked}) =>{
+const VoteCard = ({ name, img, time, id, isChecked, trigger }) => {
+  const handleCheckboxChange = () => {
+    trigger(id)
+  }
 
-
-
-
-    const handleCheckboxChange = () => {
-        // setIsChecked(!isChecked);
-        // onUpdate(!isChecked); // Call the provided update function when the state changes
-      };
-
-    return (
-        <div className ="mx-4 my-5  border-[#E2DBFF] border-[1px] bg-[#F6EEF6]  rounded-[12px] px-4 flex flex-col">
-            <div className="w-full h-[60px] flex items-center relative">
-                <img src={card2} className="w-[30px] h-[30px] rounded-[50%] "/>
-                <div className={`${styles.subheading} !text-[#000000] mx-2 font-regular !text-[12px] flex items-center h-full`} > Name </div>
-                <div className={`${styles.subheading} !text-[#000000] font-regular !text-[12px] flex items-center h-[7px] w-[7px] rounded-[50%] bg-[#939393]`} > </div>
-                <div className={`${styles.subheading} !text-[#6B6B6B] mx-2 font-regular !text-[9px] flex items-center h-full w-[50%]`} > Posted few days ago </div>
-                {/* <div className={`${styles.subheading} !font-bold !text-[10px] flex items-center px-4 py-2 bg-[#4A484B] rounded-[20px] justify-center`} > Verified </div> */}
-                <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={handleCheckboxChange}
-        className="form-checkbox h-4 w-4 bg-green-500 rounded-full" // Tailwind classes for styling
-      />
-            </div>
-            <div>
-                <img src={card2} className="w-full rounded-[20px]"/>
-            </div>
-            <div className="w-full h-[58px] flex items-center">
-            <div className=" h-[48px] flex items-center">
-            <div className={`${styles.heading2} !text-[#000000] mx-2 flex items-center h-full !text-[14px]`} > Quantity : 100 KG </div>
-
-                </div></div>
-             </div>
-    )
-
-
+  return (
+    <div className="mx-4 my-5  border-[#E2DBFF] border-[1px] bg-[#F6EEF6]  rounded-[12px] px-4 flex flex-col">
+      <div className="w-full h-[60px] flex items-center relative">
+        <img src={card2} className="w-[30px] h-[30px] rounded-[50%] " />
+        <div
+          className={`${styles.subheading} !text-[#000000] mx-2 font-regular !text-[12px] flex items-center h-full`}
+        >
+          {' '}
+          Name{' '}
+        </div>
+        <div
+          className={`${styles.subheading} !text-[#000000] font-regular !text-[12px] flex items-center h-[7px] w-[7px] rounded-[50%] bg-[#939393]`}
+        >
+          {' '}
+        </div>
+        <div
+          className={`${styles.subheading} !text-[#6B6B6B] mx-2 font-regular !text-[9px] flex items-center h-full w-[50%]`}
+        >
+          {' '}
+          Posted few days ago{' '}
+        </div>
+        {/* <div className={`${styles.subheading} !font-bold !text-[10px] flex items-center px-4 py-2 bg-[#4A484B] rounded-[20px] justify-center`} > Verified </div> */}
+        
+        <div className='w-[20%] flex justify-end'><input 
+          type="checkbox" 
+          id={`checkbox-${id}`} 
+          className="hidden" 
+          checked={isChecked}
+          onChange={handleCheckboxChange} 
+        />
+        <label
+          htmlFor={`checkbox-${id}`}
+          className={`w-6 h-6 rounded-full flex justify-center items-center cursor-pointer ${
+            isChecked ? 'bg-[#05945B] border-[#05945B]' : 'bg-gray-100 border-2 border-gray-300'
+          }`}
+        >
+          <svg
+            className={`w-4 h-4 text-white pointer-events-none ${
+              isChecked ? 'block' : 'hidden'
+            }`}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 13l4 4L19 7"
+              stroke="currentColor"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </label> </div>
+      </div>
+      <div>
+        <img src={card2} className="w-full rounded-[20px]" />
+      </div>
+      <div className="w-full h-[58px] flex items-center">
+        <div className=" h-[48px] flex items-center">
+          <div
+            className={`${styles.heading2} !text-[#000000] mx-2 flex items-center h-full !text-[14px]`}
+          >
+            {' '}
+            Quantity : 100 KG{' '}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default VoteCard
--- src/pages/VotingFeed/VotingFeed.jsx
+++ src/pages/VotingFeed/VotingFeed.jsx
@@ -2,68 +2,87 @@ import React, { useEffect, useState } from 'react'
 import styles from '../../styles/style'
 import { VoteCard } from '../../components'
 
+const VotingFeed = () => {
+  const [submits, setSubmits] = useState([
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+  ])
 
-const VotingFeed = () =>{
+  const [validates, setValidates] = useState([])
+
+  useEffect(()=>{
+   const ids = submits.map(submit => submit.id);
+   setValidates(ids)
+  },[])
+
+  const updateValidates = (id) => {
+    if (validates.includes(id)) {
+      setValidates(validates.filter(validId => validId !== id)); // Remove id
+    } else {
+      setValidates([...validates, id]); // Add id
+    }
+    //console.log(validates)
+  };
+
+  const handleValidate = () =>{
+    console.log(validates)
+  }
 
 
-   const update = (id) =>{
-
-
-   }
-
-
-    return(
-        <>
-         <div className="h-[300px] w-full bg-[#8915D0] rounded-b-[30px] relative"> 
-         <div className="absolute bottom-[25%] w-full px-8">
-        <div className={styles.flexBetween}>
-          <div
-            className={`${styles.caption1} ${styles.marginY} !text-[#B5B5B5]`}
-          >
-            <span className={`${styles.heading2} !text-[20px]`}>Digital Art Challenge</span>
-            <div className="flex gap-[10px] mt-3">
-            <div className="bg-white rounded-full py-1.5 px-1 w-[60%] flex justify-center">
-              <div className="flex ">
-               
-                <p className={`${styles.heading2} !text-[#696969] !text-[10px]`}>
-                 Creator Mary Jane
-                
-                </p>
+  return (
+    <>
+      <div className="h-[300px] w-full bg-[#8915D0] rounded-b-[30px] relative">
+        <div className="absolute bottom-[25%] w-full px-8">
+          <div className={styles.flexBetween}>
+            <div
+              className={`${styles.caption1} ${styles.marginY} !text-[#B5B5B5]`}
+            >
+              <span className={`${styles.heading2} !text-[20px]`}>
+                Digital Art Challenge
+              </span>
+              <div className="flex gap-[10px] mt-3">
+                <div className="bg-white rounded-full py-1.5 px-1 w-[60%] flex justify-center">
+                  <div className="flex ">
+                    <p
+                      className={`${styles.heading2} !text-[#696969] !text-[10px]`}
+                    >
+                      Creator Mary Jane
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-black rounded-full py-1.5 w-[40%] flex justify-center">
+                  <div className="flex">
+                    <p className={`${styles.heading2} !text-[10px]`}>
+                      8 hours left
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className={`${styles.subheading} !text-[10px] mt-2`}>
+                This is the challenge details area
               </div>
             </div>
-            <div className="bg-black rounded-full py-1.5 w-[40%] flex justify-center">
-              <div className="flex">
-               
-                <p className={`${styles.heading2} !text-[10px]`}>
-                 8 hours left
-                </p>
-              </div>
-            </div>
-            </div>
-            <div className={`${styles.subheading} !text-[10px] mt-2`}>
-              This is the challenge details area
-           
-            </div>
-         
           </div>
         </div>
       </div>
-          </div>
 
-         
-         <VoteCard/>
-        <VoteCard/>
-            
-       
-        {/* <ChallengeCard/> */}
-        
-       
-        </>
+      <div className='h-[47px] mt-3 rounded-[100px] bg-[#EDEBF3] mx-3 flex'>
+        <div className='w-[60%]'>
+        <div className={`${styles.heading2} !text-[11px] !text-black flex items-center h-full px-2 ml-3`} > Verify all challenges </div>
 
+        </div>
+        <div className='w-[40%] bg-[#202117] rounded-[100px] flex items-center justify-center'>
+        <div className={`${styles.heading2} !text-[14px] flex items-center h-full`} onClick={handleValidate} > Submit </div>
+        </div>
 
+      </div>
 
-        
-    )
+      {submits.map((item) => {
+        return <VoteCard key={item.id} name={item.name} img={item.img} time={item.time} id={item.id} isChecked={validates.includes(item.id)} trigger={updateValidates} />
+      })}
+    </>
+  )
 }
 
 export default VotingFeed
