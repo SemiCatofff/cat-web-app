c8c51132042400f530140581ee2a4a4b66db74d2
semi-infiknight <mathurshubhang2002@gmail.com>
2024-04-24 12:44:44 +0530

    ui/validator_fixes

--- src/components/VoteCard/VoteCard.jsx
+++ src/components/VoteCard/VoteCard.jsx
@@ -1,6 +1,7 @@
 import card2 from '../../assets/images/img.png'
 import styles from '../../styles/style'
 import { useState } from 'react'
+import share from '../../assets/images/shareic.png'
 
 const VoteCard = ({ name, img, time, id, isChecked, trigger }) => {
   const handleCheckboxChange = () => {
@@ -10,7 +11,7 @@ const VoteCard = ({ name, img, time, id, isChecked, trigger }) => {
   return (
     <div className="mx-4 my-5  border-[#E2DBFF] border-[1px] bg-[#F6EEF6]  rounded-[12px] px-4 flex flex-col">
       <div className="w-full h-[60px] flex items-center relative">
-        <img src={card2} className="w-[30px] h-[30px] rounded-[50%] " />
+        <img src={card2} className="w-[30px] h-[30px] rounded-[50%] " alt="" />
         <div
           className={`${styles.subheading} !text-[#000000] mx-2 font-regular !text-[12px] flex items-center h-full`}
         >
@@ -28,7 +29,8 @@ const VoteCard = ({ name, img, time, id, isChecked, trigger }) => {
           {' '}
           Posted few days ago{' '}
         </div>
-        {/* <div className={`${styles.subheading} !font-bold !text-[10px] flex items-center px-4 py-2 bg-[#4A484B] rounded-[20px] justify-center`} > Verified </div> */}
+        {/* <div className={`${styles.subheading} !font-bold !text-[10px] flex items-center px-3 py-1 bg-[#4A484B] rounded-[20px] justify-center`} >
+          <img src={tick} className='h-[22px] mr-[1px] mt-[2px]'></img> <span className='mr-1'>Verified</span> </div> */}
         
         <div className='w-[20%] flex justify-end'><input 
           type="checkbox" 
@@ -40,7 +42,7 @@ const VoteCard = ({ name, img, time, id, isChecked, trigger }) => {
         <label
           htmlFor={`checkbox-${id}`}
           className={`w-6 h-6 rounded-full flex justify-center items-center cursor-pointer ${
-            isChecked ? 'bg-[#05945B] border-[#05945B]' : 'bg-gray-100 border-2 border-gray-300'
+            isChecked ? 'bg-[#05945B] border-[#ffffff]' : 'bg-gray-100 border-2 border-gray-300'
           }`}
         >
           <svg
@@ -65,13 +67,14 @@ const VoteCard = ({ name, img, time, id, isChecked, trigger }) => {
         <img src={card2} className="w-full rounded-[20px]" />
       </div>
       <div className="w-full h-[58px] flex items-center">
-        <div className=" h-[48px] flex items-center">
+        <div className=" h-[48px] flex items-center w-full">
           <div
-            className={`${styles.heading2} !text-[#000000] mx-2 flex items-center h-full !text-[14px]`}
+            className={`${styles.heading2} !text-[#000000] mx-2 flex items-center h-full !text-[14px] w-[89%]`}
           >
             {' '}
             Quantity : 100 KG{' '}
           </div>
+          <img src={share} alt=""></img>
         </div>
       </div>
     </div>
